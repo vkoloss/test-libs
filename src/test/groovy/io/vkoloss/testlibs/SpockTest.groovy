0dceb77730747b59fe3e3f6297d97ba7d83f0fb4
Vlad Kolosnicin <vkol@ciklum.com>
2016-06-07 17:42:54 +0300

    missing

--- src/test/groovy/io/vkoloss/testlibs/SpockTest.groovy
+++ src/test/groovy/io/vkoloss/testlibs/SpockTest.groovy
@@ -13,7 +13,8 @@ class SpockTest extends Specification {
         Entity e1 = new Entity(id: 1, name: 'first', status: ACTIVE);
         Entity e2 = new Entity(id: 2, name: 'second', status: ACTIVE, parent: e1);
         Entity e3 = new Entity(id: 3, name: 'third', status: ACTIVE);
-        service.dao = [getAll: { [e1, e2, e3] }] as Dao // map coercion
+        service.dao = Mock(Dao)
+        service.dao.getAll() >> [e1, e2, e3]
 
         when:
         List<Entity> allHavingChild = service.getAllHavingParent();
